0cd0645d3251c7d9c203c78db9fb24cfb0dae7ca
Vincent Gabb <vincentgabb@gmail.com>
2020-04-02 04:27:24 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import(
+"fmt"
+)
+
+func addNumbers(input1 int, input2 int){
+ fmt.Println(input1, "*", input2, "=", input1*input2)
+}
+
+func subNumbers(input1 int, input2 int){
+ fmt.Println(input1, "/", input2, "=", input1/input2)
+}
+
+func main() {
+var num1, num2 int
+var op string
+
+ fmt.Println("Please enter a whole number between 1 and 10")
+ fmt.Scan(&num1)
+ fmt.Println("Please enter another whole number between 1 and 10")
+ fmt.Scan(&num2)
+
+ fmt.Println("Please enter a mathematical operator (+,-,/,*)")
+ fmt.Scan(&op)
+
+if(op == "*"){
+ addNumbers(num1,num2)
+ } else if(op == "/"){
+ subNumbers(num1,num2)
+ }else{
+ fmt.Println("Invalid Operator")
+ }
+}
